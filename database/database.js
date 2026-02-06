4c16255aa4432752a6616b1b31f5553bcc1033be
joão victor <joaovictorpaciello@gmail.com>
2026-02-06 11:32:30 -0300

    chore: set database timezone configuration

--- database/database.js
+++ database/database.js
@@ -4,7 +4,8 @@ const Sequelize = require ("sequelize");
 const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS,{
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect:"mysql"
+    dialect:"mysql",
+    timezone: "-03:00"
 });
 
 module.exports = connection;
