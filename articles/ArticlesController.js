a44f0962e12e3650d8b73dd43e9fd1c70e54fc82
joão victor <joaovictorpaciello@gmail.com>
2026-02-06 11:31:58 -0300

    feat: implement articles CRUD routes with category support

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,13 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify")
 
 
-router.get("/articles", (req,res)=>{
-    res.send("rota de artigos");
-})
+router.get("/admin/articles", (req, res) => {
+  res.render("admin/articles/index");
+});
 
-router.get("/admin/articles/new", (req,res) =>{
-    res.send("Rota para criar um novo artigo");
-})
+router.get("/admin/articles/new", (req, res) => {
+  Category.findAll().then((categories) => {
+    res.render("admin/articles/new" ,{
+      tinymceKey: process.env.TINYMCE_API_KEY,
+      categories: categories
+    });
+  });
+});
 
-module.exports=router;
+router.post("/articles/save", (req,res)=>{
+    var title = (req.body.title || "").trim();
+    var body = (req.body.body||"").trim();
+    var category = Number(req.body.category);
+
+
+    Article.create({
+        title: title,
+        slug: slugify(title,{
+            lower: true,
+            strict: true
+        }),
+        body: body,
+        categoryId: category
+    }).then(()=>{
+        res.redirect("/admin/articles");
+    })
+});
+
+
+module.exports = router;
